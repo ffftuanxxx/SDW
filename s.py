911d43c68f105cfe9799b0ed2118aa3060ea7116
ffftuanxxx <ffftuan000@outlook.com>
2024-05-08 14:03:29 +0800

    允许涩涩

--- s.py
+++ s.py
@@ -1,32 +1,33 @@
 import smtplib
+import mimetypes
 from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
 from email.utils import formataddr
-
-def sendemail(mail,number):
-    send_email(receivers=[str(mail)],subject="验证码",content=str(number))
-
-def send_email(receivers, subject, content):
-    """发送邮件
-
-    Args:
-        sender (str): 发件人邮箱地址
-        password (str): 发件人邮箱授权码
-        receivers (list): 收件人邮箱地址列表
-        subject (str): 邮件主题
-        content (str): 邮件内容
-
-    Returns:
-        bool: 邮件是否发送成功
-    """
-        # 读取环境变量中的敏感信息 发送邮箱账户和对应授权码
+def sendemail(receivers,content):
+    send_email([str(receivers)],"verify code",str(content),None)
+def send_email(receivers, subject, content, image_path):
+    # 读取环境变量中的敏感信息 发送邮箱账户和对应授权码
     sender = '2879907402@qq.com'
     password = 'wyectrxugvevdeca'
-    print(receivers,content)
+
     try:
         # 构造邮件对象
-        msg = MIMEText(content, 'plain', 'utf-8')
-        msg['From'] = formataddr(['From nicead.top', sender])
+        msg = MIMEMultipart()
+        msg['From'] = formataddr(['From goliathli@uic.edu.cn', sender])
         msg['Subject'] = subject
+
+        # 添加正文
+        text = MIMEText(content, 'plain', 'utf-8')
+        msg.attach(text)
+        if(image_path!=None):
+            # 添加图片附件
+            with open(image_path, 'rb') as f:
+                mime = mimetypes.guess_type(image_path)[0]
+                image = MIMEImage(f.read(), _subtype=mime)
+                image.add_header('Content-ID', '<image>')
+                image.add_header('Content-Disposition', 'attachment', filename=image_path)
+                msg.attach(image)
 
         # 连接邮箱服务器并登录
         server = smtplib.SMTP_SSL('smtp.qq.com', 465)
@@ -40,19 +41,19 @@ def send_email(receivers, subject, content):
         server.quit()
 
         return True
-    except Exception:
+    except Exception as e:
+        print(e)
         return False
 
-
 if __name__ == '__main__':
-
-    # 设置收件人列表和邮件内容
-    receivers = ['r130026041@mail.uic.edu.cn']
-    subject = '验证码'
-    content = '验证码为：咱不知道啊，咱也不会啦，咱是废物1'
+    # 设置收件人列表、邮件内容和图片路径
+    receivers = ['r130026037@mail.uic.edu.cn']
+    subject = 'Congraluation!'
+    content = 'You just recieve the offer from The University of Hong Kong (HKU), please check more detail information in attachment.'
+    image_path = '允许涩涩.jpg'  # 替换为你的图片路径
 
     # 发送邮件
-    if send_email( receivers, subject, content):
+    if send_email(receivers, subject, content, image_path):
         print('邮件发送成功')
     else:
         print('邮件发送失败')
