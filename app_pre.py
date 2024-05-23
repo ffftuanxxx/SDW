1f639b2b2f6469730b94c5c03040c3356bbc9afb
XRailgunX <107744308+ffftuanxxx@users.noreply.github.com>
2024-05-23 21:58:58 +0800

    delete mysql to prevent attack

--- app_pre.py
+++ app_pre.py
@@ -5,7 +5,7 @@ from new_control.register import Register123, session
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://llm:2501004@47.109.73.150:3306/llm'
+app.config['SQLALCHEMY_DATABASE_URI'] = ''
 
 db = SQLAlchemy(app)
 
